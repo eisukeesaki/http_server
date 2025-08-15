fd719aca26abf15aa5f59e108b87d23acdcd3c78
Eisuke Esaki <eisukeesaki@gmail.com>
2025-08-14 19:32:21 -0700

    add debug mode

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
-rustc tcp_server.rs -o server
+
+echo "rustc tcp_server.rs --cfg 'feature=\"debug\"' -o server"
+rustc tcp_server.rs --cfg 'feature="debug"' -o server
+
+# echo 'rustc tcp_server.rs -o server'
+# rustc tcp_server.rs -o server
+
+echo "./server 127.0.0.1 4242"
 ./server 127.0.0.1 4242
 
